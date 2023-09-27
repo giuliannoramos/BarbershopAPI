d82322c4e0dcc6ab8672de35381ccf89c8c07ede
Guilherme Santana <guilherme_santana@edu.univali.br>
2023-09-26 23:47:01 -0300

    Adicionando Paginação e Filtro para Order

--- src/Barbearia.Application/Contracts/Repositories/IOrderRepository.cs
+++ src/Barbearia.Application/Contracts/Repositories/IOrderRepository.cs
@@ -1,9 +1,12 @@
+using Barbearia.Application.Features;
 using Barbearia.Domain.Entities;
 
 namespace Barbearia.Application.Contracts.Repositories;
 
 public interface IOrderRepository
 {    
+    Task<(IEnumerable<Order>,PaginationMetadata)> GetAllOrdersAsync(string? searchQuery,
+         int pageNumber, int pageSize);
     Task<Order?> GetOrderByIdAsync(int orderId);
     Task<Order?> GetOrderByNumberAsync(int number);
     void DeleteOrder(Order order);    
